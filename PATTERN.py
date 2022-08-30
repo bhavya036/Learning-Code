f1c85316cda2509da8132ef144b0d23dfa3d50b9
Bhavya J Patel <85954191+bhavya036@users.noreply.github.com>
2022-08-30 23:44:44 +0530

    Update PATTERN.py

--- PATTERN.py
+++ PATTERN.py
@@ -1,7 +1,7 @@
 a = int(input("Enter the needed rows :"))
 
-for i in range (a):
-    for j in range(i+1):
-        print(j+1,end=" ")
+for i in range (a):  # For print reversing pattern take (a+1,0,-1) 
+    for j in range(i+1): # for print reversing pattern take (0,i-1)
+        print(j+1,end=" ") #for print* just replace j+1 by "*"
         
     print()
